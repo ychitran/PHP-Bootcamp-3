c2f22b2baf322cff10ca9b51aaaee93a2ed7df16
ychitran <ychi1234@gmail.com>
2021-03-25 10:16:40 +0700

    Add index timezone

--- route-demo/routes/web.php
+++ route-demo/routes/web.php
@@ -14,7 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/{timezone?}', function ($timezone = null) {
+    if (!empty($timezone)) {
+
+        // Khởi tạo giá trị giờ theo múi giờ UTC
+        $time = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone('UTC'));
+
+        // Thay đổi về múi giờ được chọn
+        $time->setTimezone(new DateTimeZone(str_replace('-', '/', $timezone)));
+
+        // Hiển thị giờ theo định dạng mong muốn
+        echo 'Múi giờ bạn chọn ' . $timezone . ' hiện tại đang là: ' . $time->format('d-m-Y H:i:s');
+    }
+    return view('index');
+});
 
 Route::get('/about', function () {
     echo "<h3>This is About Page</h3>";
